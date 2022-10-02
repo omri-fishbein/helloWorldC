f3a21a8a99e2e3f72ce598c84bcc92ac610c3cb2
omri.fishbein <omri_mop@keh.co.il>
2022-10-02 16:36:36 +0300

    commit 1 Signed-off-by: omri.fishbein <omri_mop@keh.co.il>

---
+++ src/HelloWorldC.h
@@ -0,0 +1,16 @@
+/*
+ * HelloWoldC.h
+ *
+ *  Created on: 2 срхїз 2022
+ *      Author: omri.fishbein
+ */
+
+#ifndef HELLOWORLDC_H_
+#define HELLOWORLDC_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintGreeting(char* var);
+
+#endif /* HELLOWORLDC_H_ */
