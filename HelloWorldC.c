1af4d3daf5982f8c74457b147755416e812bb1ae
omri-fishbein <114863425+omri-fishbein@users.noreply.github.com>
2022-10-03 10:27:44 +0300

    Add files via upload
    HelloWorldC first upload to github

---
+++ HelloWorldC.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : HolloWoldC.c
+ Author      : omri fishbein
+ Version     :
+ Copyright   : this is mine
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include "HelloWorldC.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World C code 1!!!"); /* prints !!!Hello WorldC!!! */
+	printf("!!!Hello World C code 2!!!"); /* prints !!!Hello WorldC!!! */
+	printf("\n!!!Hello World C code 3!!!"); /* prints !!!Hello WorldC!!! */
+	printf("\n!!!Hello World C code 4!!!"); /* prints !!!Hello WorldC!!! */
+	printf("\n!!!Hello World C code 5!!!"); /* prints !!!Hello WorldC!!! */
+
+	PrintGreeting("Israel 1");
+	PrintGreeting("Israel 2");
+	return EXIT_SUCCESS;
+}
+
+void PrintGreeting(char* var)
+{
+	printf("\nhello, %s", var);
+}//PrintGreeting(...)
